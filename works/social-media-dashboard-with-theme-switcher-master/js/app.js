71e65628f93b81a198f0621b405c51f1e641f63a
Basyaviy <basyaviy@mail.ru>
2021-10-11 10:15:07 +0300

    correcting app.js

--- works/social-media-dashboard-with-theme-switcher-master/js/app.js
+++ works/social-media-dashboard-with-theme-switcher-master/js/app.js
@@ -1,31 +1,8 @@
 const lightArr = document.querySelectorAll("#color");
-var currentColor="dark";
 
-function toggleMode()
-{
-    if(currentColor==="light"){
-        toDarkMode();
-    }else{
-        toLightMode();
-    }
-}
-
-
-function toLightMode(){
-    const lightArr = document.querySelectorAll("#color");
+function toggleMode(){
     for(var i=0;i<lightArr.length;i++){
-        var el=lightArr[i];        
-        el.classList.add('light');
-        el.classList.remove('dark');
+        var el=lightArr[i];    
+        el.classList.toggle("dark");
     }
-    currentColor="light";
-}
-
-function toDarkMode(){
-    for(var i=0;i<lightArr.length;i++){
-        var el=lightArr[i];        
-        el.classList.add('dark');
-        el.classList.remove('light');
-    }
-    currentColor="dark";
 }
